d5e2c8ab128041f713979182b6e643cca5b37121
vdmkenny <vdmkenny@inuits.eu>
2016-03-11 10:47:05 +0100

    templated config filewq

---
+++ manifests/config.pp
@@ -0,0 +1,11 @@
+define thruk::config(
+    $servers=['127.0.0.1'],
+    $titleprefix= "Thruk - "
+   ){
+    
+#    file{'/etc/thruk/thruk_local.conf':
+#	owner  => root,
+#	content => template('thruk/thruk_local.conf.erb'),
+#        }
+
+    }
--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,6 @@ class thruk {
     package{"thruk":
         ensure=>present,
         notify=>Service['httpd'],
-        require=>Yumrepo['base'],
     }
 
     
@@ -12,7 +11,10 @@ class thruk {
             mode   => 644,
             owner  => apache,
             group  => apache,
-            source => "puppet:///modules/thruk/thruk_local.conf",
+           # source => "puppet:///modules/thruk/thruk_local.conf",
+            content => template('thruk/thruk_local.conf.erb'),
+            notify  => Service['thruk'],
+            require => Package['thruk'],
         }
               
         service{"httpd":
